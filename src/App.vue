ca68e1a19ddef43c7362f9165204250e90bc2015
Johnny Zou <294763483@qq.com>
2019-06-18 21:29:27 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <transition>
+      <keep-alive exclude="Detail">
+        <router-view/>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
