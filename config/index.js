f7958b83320c123b753891772674ce54965c824c
Johnny Zou <294763483@qq.com>
2019-06-18 23:23:07 +0800

    bugDIx

--- config/index.js
+++ config/index.js
@@ -59,7 +59,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/travel/dist',
 
     /**
      * Source Maps
